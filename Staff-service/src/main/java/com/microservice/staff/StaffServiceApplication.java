0de098c75d68193b9c0cbcc5c056f5c5f5b2b19e
manikanta <62785996+Manikanta7517@users.noreply.github.com>
2022-11-04 11:14:14 +0530

    Add files via upload

---
+++ Staff-service/src/main/java/com/microservice/staff/StaffServiceApplication.java
@@ -0,0 +1,16 @@
+package com.microservice.staff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class StaffServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffServiceApplication.class, args);
+	}
+	
+
+}
